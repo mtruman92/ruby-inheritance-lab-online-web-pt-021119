33a4ed6ebea4ed87e33b032f211d5e4b1c643fd1
Mickae Truman <mtruman@nyit.edu>
2019-03-22 12:31:13 +0000

    Automatically backed up by Learn

--- lib/user.rb
+++ lib/user.rb
@@ -17,4 +17,12 @@ def initialize(first_name,last_name)
     @first_name = first_name
   end
   
+  def last_name
+    @last_name
+  end
+  
+  def last_name=(last_name)
+    @last_name = last_name
+  end
+  
 end
